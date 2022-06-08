826d5b44a7798be7338203e0e7293ce0bd252d8c
keemyereem <bmongk0815@gmail.com>
2022-06-08 18:33:07 +0900

    220608

--- js/common.js
+++ js/common.js
@@ -4,7 +4,10 @@ $(function(){
 
 var mainEvent = {
     init:function(){
-       this.mainVisual();417
+       this.mainVisual();
+       this.s1Tab();
+       this.s2Tab();
+       this.floating();
     },
 
     mainVisual:function(){
@@ -12,27 +15,58 @@ var mainEvent = {
             observer: true,
             observeParents: true,
             slidesPerView : 1,
-            speed: 1000,
-            
+            speed: 500,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false, 
+            },
+
+
             navigation: {
                 nextEl: '.arrow.swiper-button-next',
                 prevEl: '.arrow.swiper-button-prev',
             },
-            watchOverflow: true,
-
-            breakpoints: {
-
-                768: {
-                slidesPerView: 1,  //브라우저가 768보다 클 때
-                spaceBetween: 20,
-                },
-                1024: {
-                slidesPerView: 2,  //브라우저가 1024보다 클 때
-                spaceBetween: 70,
-                },
-            },   
+            watchOverflow: true, 
         });
     },
+
+    s1Tab: function(){
+        var Tabs = $('.cont_main .section1 .s1_tab li');
+        Tabs.on("click", function() {
+            $(this).addClass('on');
+            $(this).siblings().removeClass('on');
+            
+            var Tabs_cont = Tabs.index(this)+1;
+            $('.cont_main .section1 .tab_contents').removeClass('on');
+            $('.cont_main .section1 .tab_content0' + Tabs_cont).addClass('on');
+        });
+    },
+
+    s2Tab: function(){
+        var Tabs = $('.cont_main .section2 .s2_tab li');
+        Tabs.on("click", function() {
+            $(this).addClass('on');
+            $(this).siblings().removeClass('on');
+            
+            var Tabs_cont = Tabs.index(this)+1;
+            $('.cont_main .section2 .tab_contents').removeClass('on');
+            $('.cont_main .section2 .tab_content0' + Tabs_cont).addClass('on');
+        });
+    },
+
+    floating:function() {
+        $(window).scroll(function() {
+            var s3Top = $('.cont_main .section3').offset().top - $(window).outerHeight() + Number(250);
+            if($(this).scrollTop() > s3Top){
+                $('.floating_area').addClass('on').css({'bottom': '50rem'});
+
+            }else {
+                $('.floating_area').removeClass('on').css({'bottom':'4rem'});
+            }
+        });
+
+    },
+
 
 };
 
