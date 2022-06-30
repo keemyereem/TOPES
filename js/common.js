1bbc239d8aac55803361e29b3440ac22b2589e0d
keemyereem <bmongk0815@gmail.com>
2022-06-30 18:34:42 +0900

    220630
    Co-authored-by: Woo Hae Min <WooHaem@users.noreply.github.com>

--- js/common.js
+++ js/common.js
@@ -8,6 +8,7 @@ $(function(){
         startEvent: "load",
 
 	});
+
 });
 
 
@@ -21,6 +22,7 @@ var commonEvent = {
        this.goTopEvent();
        this.bgAni();
        this.tabFunction();
+       this.fnImgPop();
     }, 
 
     headerEvent:function(){
@@ -126,6 +128,14 @@ var commonEvent = {
             }, 100)
         })
 
+    },
+
+    fnImgPop: function() {
+        $(document).on('click', '.view', function() {
+            var getUrl = $(this).siblings('.img_m').attr('src');
+            getUrl = getUrl.replace(".png","_view.png");
+            window.open(getUrl) 
+        });
     },
 
 }
@@ -338,22 +348,16 @@ var companyEvent={
     },
 
     history: function() {
-
-        // 연혁 탭버튼 누르면 슬라이드 이동 스크립트 -> html 소스 하단에 위치
-        // function fnMove(seq){
-        //     var id = $('#'+ seq);
-        //     var offset = id.offset();
-        //     $('html, body').animate({scrollTop : offset.top - 169}, 400);
-        // }
         
         // 연혁 인터랙션
         $(document).ready(function () {
             var section = $('._historySection');
-            var sectionLength = section.length;
+            var sectionOn = $('._historySection.on');
+            let sectionLength = section.length;
             
             // 연도와 내용 높이값 맞추기
             section.find('.desc-info').each(function (idx) {
-                var sectionHeight = section.eq(idx).find('.desc-info').height();
+                let sectionHeight = section.eq(idx).find('.desc-info').height();
                 section.eq(idx).find('.year-info').css('height', sectionHeight / 10 + 'rem')
             })
 
@@ -365,48 +369,33 @@ var companyEvent={
                      setHistoryScroll(section.eq(i));
                 }
 
-                if ($('.tab_contents .year-info').find('li:last-child').hasClass('active')) {
-                    $('.tab_contents .desc-info').find('ul:last-child').addClass('active');
-                    $('.tab_contents .desc-info').find('ul:not(:last-child)').removeClass('active');
+                // 연혁 연도 마지막 active일 때, 내용 마지막 active 또는 스크롤 맨 끝 도달 시
+                if ($('.tab_contents.on .year-info').find('li:last-child').hasClass('active') || $(window).scrollTop() == scrollEnd) {
+                    $('.tab_contents.on .year-info').find('li:last-child').addClass('active');
+                    $('.tab_contents.on .year-info').find('li:not(:last-child)').removeClass('active');
+                    $('.tab_contents.on .desc-info').find('ul:last-child').addClass('active');
+                    $('.tab_contents.on .desc-info').find('ul:not(:last-child)').removeClass('active');
                 }
 
                 // 반응형 연혁 하단 여백 생성
                 if($(window).width() > 1920){
                     $('.desc-info').css('padding-bottom','350px');
-                } else if ($(window).width() < 768 && $(window).height() > 670) {
+                } else if ($(window).width() < 768 && $(window).height() > 770) {
                     $('.desc-info').css('padding-bottom','50px');
                 } else {
                     $('.desc-info').css('padding-bottom','0');
                 }
-
-                // if($(window).scrollTop() == scrollEnd) {
-                //     $('.tab_contents .year-info').find('li').removeClass('active');
-                //     $('.tab_contents .year-info').find('li:last-child').addClass('active');
-                //     $('.tab_contents .desc-info').find('ul').removeClass('active');
-                //     $('.tab_contents .desc-info').find('ul:last-child').addClass('active');
-                //     console.log(huddleLastcont)
-                // }
-                // else {
-                //     $('.tab_contents:last-child .year-info').find('li:last-child').removeClass('active')
-                //     $('.tabs li').on("click", function() {
-                //         var Tabs_cont = $('.tabs li').index(this)+1;
-                //         $('.tab_contents .year-info').find('li').removeClass('active');
-                //         $('.tab_contents .desc-info').find('ul').removeClass('active');
-                //         $('.tab_contents .desc-info').find('ul:last-child').removeClass('active');
-                //         $('.tab_content0' + Tabs_cont).find('li:first-child').addClass('active');
-                //         $('.tab_content0' + Tabs_cont).find('ul:first-child').addClass('active');
-                //     });
-                // }
             })
         
             function setHistoryScroll($information) {
-                var gap = 20; 
-                var gapYear = 69;
-                var currentPosition = $(window).scrollTop() + 175; /*+95 X*/
                 var sectionOffset = $information.find('.desc-info ul').eq(0).offset().top;
-                // var scrollStart = currentPosition - sectionOffset + 60;
                 var size = $information.find('.year-info li').length;
-                var getFixedMargin = 18;
+                let gap = 20; 
+                let gapYear = 69;
+                let currentPosition = $(window).scrollTop() + 175;
+                let getFixedMargin = 18;
+                let lastUl = $information.find('.desc-info ul').last(); // 연혁 마지막 내용 위치값
+                let lastBottom = lastUl.offset().top + lastUl.height(); // 연혁 마지막 내용 끝 지점
 
                 $information.find('.year-info li').each(function (index) {
                     // 반응형 변수값 교체
@@ -415,13 +404,13 @@ var companyEvent={
                         currentPosition = $(window).scrollTop() + 110;
                         getFixedMargin = 12; 
                     }
-                    
-                    if (currentPosition < $information.find('.desc-info ul').eq(0).offset().top - gap) {
+
+                    if (currentPosition < sectionOffset - gap) {
                         //섹션 이전 화면에서는 absolute상태
                         $information.find('.year-info').css({'top' : 'auto', 'position' : 'absolute', 'margin-top': '0'});
                         $information.find('.year-info ul').css({'position': 'relative', 'margin-top': '0'});
-                            
-                        
+
+                        // 반응형 태블릿 연도 좌우 고정풀기
                         if($(window).width()<=1400 && $(window).width()>767){
                             $(window).scroll(function(){
                                 $(".year-info").css("left",0 - $(this).scrollLeft());
@@ -432,28 +421,36 @@ var companyEvent={
                                 $(".year-info").css("left","initial");
                             });
                         };
-                    
+                        
+                    } else if ($information.hasClass('on') && currentPosition > lastBottom && $(window).height() < 851) {
+                        // 연혁 내용 마지막 위치값 이상 넘어갈 경우 연도 고정
+                        $information.find('.year-info').css({'top' : 'auto', 'bottom': '0', 'position' : 'absolute', 'margin-top': '0'});
+                        $information.find('.year-info ul').css({'position': 'absolute', 'bottom': '5rem'});
+                        $('.tab_contents.on .year-info').find('li:last-child').addClass('active');
+                        $('.tab_contents.on .year-info').find('li:not(:last-child)').removeClass('active');
+
                     } else {
                         //섹션 안으로 들어오면 fixed 상태
                         if (size !== index + 1) {
                             if (currentPosition > $information.find('.desc-info ul').eq(index).offset().top - gap && currentPosition < $information.find('.desc-info ul').eq(index + 1).offset().top - gap) {
-                                $information.find('.year-info').css({'top': '0', 'position': 'fixed', 'margin-top': '' + getFixedMargin + 'rem'});
+                                $information.find('.year-info').css({'top': '0', 'bottom': 'auto', 'position': 'fixed', 'margin-top': '' + getFixedMargin + 'rem'});
                                 $information.find('.year-info ul').css({'margin-top': '-' + (gapYear * index) + 'px', 'position': 'relative', 'bottom': 'unset'});
                                 $information.find('.year-info li').eq(index).addClass('active').siblings().removeClass('active');
                                 $information.find('.desc-info ul').eq(index).addClass('active').siblings().removeClass('active');
                             }
+                            
                         } else {
                             if (currentPosition > $information.find('.desc-info ul').eq(index).offset().top) {
-                                // $information.find('.year-info').css({'top': scrollStart + gap - (gapYear * index)}, {'position': 'fixed'});
-                                $information.find('.year-info').css({'top': '0', 'position': 'fixed', 'margin-top': '' + getFixedMargin + 'rem'});
-                                $information.find('.year-info ul').css({'margin-top': '-' + (gapYear * index) + 'px'});
+                                // 연혁 내용 마지막 위치값 안으로 진입할 경우 연도 fixed
+                                if (currentPosition < lastBottom) {
+                                    $information.find('.year-info').css({'top': '0', 'bottom': 'auto', 'position': 'fixed', 'margin-top': '' + getFixedMargin + 'rem'});
+                                    $information.find('.year-info ul').css({'margin-top': '-' + (gapYear * index) + 'px', 'position': 'relative', 'bottom': 'unset'});
+                                }
                                 $information.find('.year-info li').eq(index).addClass('active').siblings().removeClass('active');
                             }
                         }
                     }
 
-                    
-                    
                 })
             }
         })
